fb0c62d9bceb026bfdfc73c534b928ef693d0631
Mohammad Kayyali @yahoo <kayyaliemail@yahoo.com>
2023-06-12 11:59:04 +0100

    test1

---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		[Required]
+		public String Name { get; set; }
+		[DisplayName("Order")]
+		public int DisplayOrder { get; set; }
+
+	}
+}
+
